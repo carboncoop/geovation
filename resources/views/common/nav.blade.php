b27eaa070a2b3f3dca5bdf4e03de4a9a12d21e6b
cagabi <cagabi@lapiluka.org>
2017-05-25 16:54:54 +0100

    Changes in email content when clicking Contact

--- resources/views/common/nav.blade.php
+++ resources/views/common/nav.blade.php
@@ -4,7 +4,7 @@
 		<!-- <li><a href="/styles">Styles</a></li> -->
 		<!-- <li><a href="/results">Results</a></li> -->
 		<li><a href="/how-it-works">How it works</a></li>
-		<li><a href="mailto:info@carbon.coop">Contact</a></li>
+		<li><a href="mailto:info@carbon.coop?subject=MyHomeEnergy Salford&amp;body=Hello,%0D%0D I am using the tool and...">Contact</a></li>
 		<!-- <li><a href="/form">Form</a></li> -->
 	</ul>
 </nav>
