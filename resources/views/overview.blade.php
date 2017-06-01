db2e6723b3f80a38fd0cf29a4a6ec8a5836ad41b
cagabi <cagabi@lapiluka.org>
2017-06-01 10:32:17 +0100

    text format changes

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -3,106 +3,106 @@
 @section('title', 'Overview')
 
 @section('content')
-	<div class="page-wrapper--thin">
-		<div class="page-title page-title--icon page-title--small-bot-margin">
-			<div class="page-title__icon">
-				<img src="{{ URL::asset('img/icons/overview.png') }}" alt="">
-			</div>
-			<h1>My Home Energy Overview</h1>
-		</div>
-		<div class="page-intro">
-			<h2 class="page-sub-title">59 Lever Street, Manchester M1 1EA</h2>
-		</div>
-	</div>
-	<div class="page-wrapper">
-		<div class="grid cf overview-results">
-			<div class="col-3">
-				<img class="overview-results__graph" src="{{ URL::asset('img/temp-graph.png') }}" alt="">
-			</div>
-			<div class="col-3">
-				<img class="overview-results__graph" src="{{ URL::asset('img/temp-graph.png') }}" alt="">
-			</div>
-			<div class="col-3">
-				<img class="overview-results__graph" src="{{ URL::asset('img/temp-graph.png') }}" alt="">
-			</div>
-		</div>
-		<div class="ctext overview-sharing body-copy body-copy--dbl-margin">
-			<a href="#" class="btn btn--basic btn--highlighted">Email my results</a>
-			<p>You can share or print your My Home Energy results</p>
-			<a href="#" class="btn btn--round btn--icon btn--icon-print">Share</a>
-			<a href="#" class="btn btn--round btn--icon btn--icon-print">Print</a>
-		</div>
-	</div>
-	<div class="grey-bg overview-more-info">
-		<div class="page-wrapper">
-			<div class="grid grid--double-pad cf">
-				<div class="col-4 body-copy body-copy--dbl-margin">
-					<h3 class="line-accent">Salford Council</h3>
-					<p>The local authority for the city of Salford, Salford Council provided the data license that enables this site to operate.</p>
-					<a href="http://www.salford.gov.uk/" target="_blank" class="btn btn--basic btn--highlighted">Find out more</a>
-				</div>
-				<div class="col-4 body-copy body-copy--dbl-margin">
-					<h3 class="line-accent">URBED</h3>
-					<p>Technical partners and retrofit experts, URBED specialise in urban design and sustainability in an urban context</p>
-					<a href="http://www.urbed.coop/" target="_blank" class="btn btn--basic btn--highlighted">Find out more</a>
-				</div>
-				<div class="col-4 body-copy body-copy--dbl-margin">
-					<h3 class="line-accent">Geovation Challenge</h3>
-					<p>A challenge competition for start-ups wanting to help solve pressing issues using location information.</p>
-					<a href="https://geovation.uk/" target="_blank" class="btn btn--basic btn--highlighted">Find out more</a>
-				</div>
-				<div class="col-4 body-copy body-copy--dbl-margin">
-					<h3 class="line-accent">Fieldwork</h3>
-					<p>A collaborative design agency working on everything from digital platforms to brand identities</p>
-					<a href="http://madebyfieldwork.com/" target="_blank" class="btn btn--basic btn--highlighted">Find out more</a>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="overview-case-studies">
-		<div class="page-wrapper--thin">
-			<div class="page-title page-title--icon-overlap page-title--small-bot-margin page-title--no-border">
-				<div class="page-title__icon">
-					<img src="{{ URL::asset('img/icons/case-studies.png') }}" alt="">
-				</div>
-				<h1>Case Studies</h1>
-			</div>
-		</div>
+<div class="page-wrapper--thin">
+    <div class="page-title page-title--icon page-title--small-bot-margin">
+        <div class="page-title__icon">
+            <img src="{{ URL::asset('img/icons/overview.png') }}" alt="">
+        </div>
+        <h1>My Home Energy Overview</h1>
+    </div>
+    <div class="page-intro">
+        <h2 class="page-sub-title">59 Lever Street, Manchester M1 1EA</h2>
+    </div>
+</div>
+<div class="page-wrapper">
+    <div class="grid cf overview-results">
+        <div class="col-3">
+            <img class="overview-results__graph" src="{{ URL::asset('img/temp-graph.png') }}" alt="">
+        </div>
+        <div class="col-3">
+            <img class="overview-results__graph" src="{{ URL::asset('img/temp-graph.png') }}" alt="">
+        </div>
+        <div class="col-3">
+            <img class="overview-results__graph" src="{{ URL::asset('img/temp-graph.png') }}" alt="">
+        </div>
+    </div>
+    <div class="ctext overview-sharing body-copy body-copy--dbl-margin">
+        <a href="#" class="btn btn--basic btn--highlighted">Email my results</a>
+        <p>You can share or print your My Home Energy results</p>
+        <a href="#" class="btn btn--round btn--icon btn--icon-print">Share</a>
+        <a href="#" class="btn btn--round btn--icon btn--icon-print">Print</a>
+    </div>
+</div>
+<div class="grey-bg overview-more-info">
+    <div class="page-wrapper">
+        <div class="grid grid--double-pad cf">
+            <div class="col-4 body-copy body-copy--dbl-margin">
+                <h3 class="line-accent">Salford Council</h3>
+                <p>The local authority for the city of Salford, Salford Council provided the data license that enables this site to operate.</p>
+                <a href="http://www.salford.gov.uk/" target="_blank" class="btn btn--basic btn--highlighted">Find out more</a>
+            </div>
+            <div class="col-4 body-copy body-copy--dbl-margin">
+                <h3 class="line-accent">URBED</h3>
+                <p>Technical partners and retrofit experts, URBED specialise in urban design and sustainability in an urban context</p>
+                <a href="http://www.urbed.coop/" target="_blank" class="btn btn--basic btn--highlighted">Find out more</a>
+            </div>
+            <div class="col-4 body-copy body-copy--dbl-margin">
+                <h3 class="line-accent">Geovation Challenge</h3>
+                <p>A challenge competition for start-ups wanting to help solve pressing issues using location information.</p>
+                <a href="https://geovation.uk/" target="_blank" class="btn btn--basic btn--highlighted">Find out more</a>
+            </div>
+            <div class="col-4 body-copy body-copy--dbl-margin">
+                <h3 class="line-accent">Fieldwork</h3>
+                <p>A collaborative design agency working on everything from digital platforms to brand identities</p>
+                <a href="http://madebyfieldwork.com/" target="_blank" class="btn btn--basic btn--highlighted">Find out more</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="overview-case-studies">
+    <div class="page-wrapper--thin">
+        <div class="page-title page-title--icon-overlap page-title--small-bot-margin page-title--no-border">
+            <div class="page-title__icon">
+                <img src="{{ URL::asset('img/icons/case-studies.png') }}" alt="">
+            </div>
+            <h1>Case Studies</h1>
+        </div>
+    </div>
 
-		<div class="page-wrapper">
-			<div class="grid cf">
-				<div class="col-3 body-copy body-copy--dbl-margin">
-					<div class="case-study">
-						<img src="{{ URL::asset('img/case-studies/dominic-mccann.png') }}" class="case-study__thumb" alt="">
-						<h3 class="line-accent highlighted">Dominic McCann - the Victorian Terrace whole house retrofit</h3>
-						<p><b>Case Study:</b> With the help of Carbon Co-op, Dominic made big changes to the fabric of his home, adding external wall insulation, triple glazed windows, underfloor insulation and solar panels.</p>
-						<a href="https://vimeo.com/142379756" target="_blank" class="btn btn--round btn--highlighted">Find out more</a>
-					</div>
-				</div>
-				<div class="col-3 body-copy body-copy--dbl-margin">
-					<div class="case-study">
-						<img src="{{ URL::asset('img/case-studies/lorenza-casini.png') }}" class="case-study__thumb" alt="">
-						<h3 class="line-accent highlighted">Lorenza Casini - slow and steady improvements over time</h3>
-						<p><b>Case Study:</b> Lorenza and Paul have made step-by-step improvements over a number of years, using DIY and professional help including adding roof, floor and walls insulation and triple glazed windows.</p>
-						<a href="https://vimeo.com/142238564" target="_blank" class="btn btn--round btn--highlighted">Find out more</a>
-					</div>
-				</div>
-				<div class="col-3 body-copy body-copy--dbl-margin">
-					<div class="case-study">
-						<img src="{{ URL::asset('img/case-studies/gervase-mangwana.png') }}" class="case-study__thumb" alt="">
-						<h3 class="line-accent highlighted">Gervase Mangwana - taking the house apart and putting it back together again</h3>
-						<p><b>Case Study:</b> Gervase took on the project of a whole house retrofit, planning and carrying out the work himself. This extensive work involved many different improvements including solid wall insulation and a heat recovery ventilation system.</p>
-						<a href="https://vimeo.com/142371233" target="_blank" class="btn btn--round btn--highlighted">Find out more</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="overview-contact-us">
-		<div class="page-wrapper--x-thin page-intro body-copy body-copy--dbl-margin">
-			<h2 class="page-sub-title">59 Lever Street, Manchester M1 1EA</h2>
-			<p>For more information contact Carbon Co-op at: <a href="mailto:info@carbon.coop">info@carbon.coop</a></p>
-			<a href="mailto:info@carbon.coop" class="btn btn--basic btn--highlighted">Get In touch</a>
-		</div>
-	</div>
+    <div class="page-wrapper">
+        <div class="grid cf">
+            <div class="col-3 body-copy body-copy--dbl-margin">
+                <div class="case-study">
+                    <img src="{{ URL::asset('img/case-studies/dominic-mccann.png') }}" class="case-study__thumb" alt="">
+                    <h3 class="line-accent highlighted">Dominic McCann - the Victorian Terrace whole house retrofit</h3>
+                    <p><b>Case Study:</b> With the help of Carbon Co-op, Dominic made big changes to the fabric of his home, adding external wall insulation, triple glazed windows, underfloor insulation and solar panels.</p>
+                    <a href="https://vimeo.com/142379756" target="_blank" class="btn btn--round btn--highlighted">Find out more</a>
+                </div>
+            </div>
+            <div class="col-3 body-copy body-copy--dbl-margin">
+                <div class="case-study">
+                    <img src="{{ URL::asset('img/case-studies/lorenza-casini.png') }}" class="case-study__thumb" alt="">
+                    <h3 class="line-accent highlighted">Lorenza Casini - slow and steady improvements over time</h3>
+                    <p><b>Case Study:</b> Lorenza and Paul have made step-by-step improvements over a number of years, using DIY and professional help including adding roof, floor and walls insulation and triple glazed windows.</p>
+                    <a href="https://vimeo.com/142238564" target="_blank" class="btn btn--round btn--highlighted">Find out more</a>
+                </div>
+            </div>
+            <div class="col-3 body-copy body-copy--dbl-margin">
+                <div class="case-study">
+                    <img src="{{ URL::asset('img/case-studies/gervase-mangwana.png') }}" class="case-study__thumb" alt="">
+                    <h3 class="line-accent highlighted">Gervase Mangwana - taking the house apart and putting it back together again</h3>
+                    <p><b>Case Study:</b> Gervase took on the project of a whole house retrofit, planning and carrying out the work himself. This extensive work involved many different improvements including solid wall insulation and a heat recovery ventilation system.</p>
+                    <a href="https://vimeo.com/142371233" target="_blank" class="btn btn--round btn--highlighted">Find out more</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="overview-contact-us">
+    <div class="page-wrapper--x-thin page-intro body-copy body-copy--dbl-margin">
+        <h2 class="page-sub-title">59 Lever Street, Manchester M1 1EA</h2>
+        <p>For more information contact Carbon Co-op at: <a href="mailto:info@carbon.coop">info@carbon.coop</a></p>
+        <a href="mailto:info@carbon.coop" class="btn btn--basic btn--highlighted">Get In touch</a>
+    </div>
+</div>
 @stop
--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -5,157 +5,157 @@
 @section('content')
 
 <div class="page-wrapper--thin">
-	<div class="page-title page-title--icon page-title--small-bot-margin">
-		<div class="page-title__icon">
-			<img src="{{ URL::asset('img/icons/results.png') }}" alt="">
-		</div>
-		<h1>My Home Energy Results</h1>
-	</div>
-	<div class="page-intro">
-		<h2 class="page-sub-title">{{ $osAddress->humanReadableAddress()}}, {{ $osAddress->postcode }}</h2>
-	</div>
+    <div class="page-title page-title--icon page-title--small-bot-margin">
+        <div class="page-title__icon">
+            <img src="{{ URL::asset('img/icons/results.png') }}" alt="">
+        </div>
+        <h1>My Home Energy Results</h1>
+    </div>
+    <div class="page-intro">
+        <h2 class="page-sub-title">{{ $osAddress->humanReadableAddress()}}, {{ $osAddress->postcode }}</h2>
+    </div>
 </div>
 
 <div class="page-wrapper">
-	<div class="grid cf grid--two-col">
-		<div class="col-2">
-			<div class="house-illustration">
-				<a data-modal-trigger="fabric-modal" data-info-ref="insulation-info" href="#" class="house-illustration__icon--insulation">
-					<img src="{{ URL::asset('img/icons/insulation.png') }}" alt="">
-					<img src="{{ URL::asset('img/icons/insulation-white.png') }}" class="white-hover" alt="">
-				</a>
-				<a data-modal-trigger="services-modal" data-info-ref="services-info" href="#" class="house-illustration__icon--services">
-					<img src="{{ URL::asset('img/icons/services.png') }}" alt="">
-					<img src="{{ URL::asset('img/icons/services-white.png') }}" class="white-hover" alt="">
-				</a>
-				<!-- <a data-modal-trigger="roof-modal" data-info-ref="data-info" href="#" class="house-illustration__icon--data">
-					<img src="{{ URL::asset('img/icons/data.png') }}" alt="">
-					<img src="{{ URL::asset('img/icons/data-white.png') }}" class="white-hover" alt="">
-				</a> -->
-				<a data-modal-trigger="energy-modal" data-info-ref="energy-info" href="#" class="house-illustration__icon--energy">
-					<img src="{{ URL::asset('img/icons/energy.png') }}" alt="">
-					<img src="{{ URL::asset('img/icons/energy-white.png') }}" class="white-hover" alt="">
-				</a>
-				<img src="{{ URL::asset('img/house-illustration.svg') }}" alt="">
-			</div>
+    <div class="grid cf grid--two-col">
+        <div class="col-2">
+            <div class="house-illustration">
+                <a data-modal-trigger="fabric-modal" data-info-ref="insulation-info" href="#" class="house-illustration__icon--insulation">
+                    <img src="{{ URL::asset('img/icons/insulation.png') }}" alt="">
+                    <img src="{{ URL::asset('img/icons/insulation-white.png') }}" class="white-hover" alt="">
+                </a>
+                <a data-modal-trigger="services-modal" data-info-ref="services-info" href="#" class="house-illustration__icon--services">
+                    <img src="{{ URL::asset('img/icons/services.png') }}" alt="">
+                    <img src="{{ URL::asset('img/icons/services-white.png') }}" class="white-hover" alt="">
+                </a>
+                <!-- <a data-modal-trigger="roof-modal" data-info-ref="data-info" href="#" class="house-illustration__icon--data">
+                        <img src="{{ URL::asset('img/icons/data.png') }}" alt="">
+                        <img src="{{ URL::asset('img/icons/data-white.png') }}" class="white-hover" alt="">
+                </a> -->
+                <a data-modal-trigger="energy-modal" data-info-ref="energy-info" href="#" class="house-illustration__icon--energy">
+                    <img src="{{ URL::asset('img/icons/energy.png') }}" alt="">
+                    <img src="{{ URL::asset('img/icons/energy-white.png') }}" class="white-hover" alt="">
+                </a>
+                <img src="{{ URL::asset('img/house-illustration.svg') }}" alt="">
+            </div>
 
-			<div class="house-illustration-info body-copy insulation-info">
-				<b>Fabric/Insulation:</b>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum optio aperiam sapiente laboriosam quaerat dolor cumque. Blanditiis veniam rerum odio!</p>
-			</div>
-			<div class="house-illustration-info body-copy services-info">
-				<b>Services:</b>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi possimus, blanditiis laboriosam temporibus quo nulla vitae debitis soluta impedit. Aliquam.</p>
-			</div>
-			<div class="house-illustration-info body-copy data-info">
-				<b>Home Data:</b>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, doloremque facere sequi labore officia delectus in qui assumenda voluptas, eius.</p>
-			</div>
-			<div class="house-illustration-info body-copy energy-info">
-				<b>Current Energy Use:</b>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas id, iste repellendus cumque voluptates dolores sequi. Rerum cupiditate quo, optio?</p>
-			</div>
-		</div>
-		<div class="col-2 results-graphs">
-			<div class="body-copy home-instructions">
-				<p><b>Instructions:</b> The graphs here show the results for your home. Adjust the different aspects of its construction and services to see what changes you can make.</p>
-			</div>
+            <div class="house-illustration-info body-copy insulation-info">
+                <b>Fabric/Insulation:</b>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum optio aperiam sapiente laboriosam quaerat dolor cumque. Blanditiis veniam rerum odio!</p>
+            </div>
+            <div class="house-illustration-info body-copy services-info">
+                <b>Services:</b>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi possimus, blanditiis laboriosam temporibus quo nulla vitae debitis soluta impedit. Aliquam.</p>
+            </div>
+            <div class="house-illustration-info body-copy data-info">
+                <b>Home Data:</b>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, doloremque facere sequi labore officia delectus in qui assumenda voluptas, eius.</p>
+            </div>
+            <div class="house-illustration-info body-copy energy-info">
+                <b>Current Energy Use:</b>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas id, iste repellendus cumque voluptates dolores sequi. Rerum cupiditate quo, optio?</p>
+            </div>
+        </div>
+        <div class="col-2 results-graphs">
+            <div class="body-copy home-instructions">
+                <p><b>Instructions:</b> The graphs here show the results for your home. Adjust the different aspects of its construction and services to see what changes you can make.</p>
+            </div>
 
-			@foreach ($resultsData['preferences'] as $preference)
-				@include('graphs.' . $preference, array('resultsData' => $resultsData ))
-			@endforeach
+            @foreach ($resultsData['preferences'] as $preference)
+            @include('graphs.' . $preference, array('resultsData' => $resultsData ))
+            @endforeach
 
-		</div>
-	</div>
-	<a href="/overview" class="btn btn--basic btn--highlighted">Find out more</a>
-	<div class="reset-nav">
-		<a href="#" class="js-return reset-nav__back">Back</a>
-		<a href="/" class="reset-nav__reset">Start Again</a>
-	</div>
+        </div>
+    </div>
+    <a href="/overview" class="btn btn--basic btn--highlighted">Find out more</a>
+    <div class="reset-nav">
+        <a href="#" class="js-return reset-nav__back">Back</a>
+        <a href="/" class="reset-nav__reset">Start Again</a>
+    </div>
 </div>
 
 <div class="modal" data-modal-id="fabric-modal">
-	<form action="#">
-		<div class="grid grid--double-pad cf">
-			<div class="form-divider cf">
-				<h3>Fabric and Construction</h3>
-				<div class="form-divider__icon form-divider__icon--modal">
-					<img src="{{ URL::asset('img/icons/insulation.png') }}" alt="">
-				</div>
-			</div>
+    <form action="#">
+        <div class="grid grid--double-pad cf">
+            <div class="form-divider cf">
+                <h3>Fabric and Construction</h3>
+                <div class="form-divider__icon form-divider__icon--modal">
+                    <img src="{{ URL::asset('img/icons/insulation.png') }}" alt="">
+                </div>
+            </div>
 
-			@include('form-sections.fabric',  array('pageLocation' => 'results'))
+            @include('form-sections.fabric',  array('pageLocation' => 'results'))
 
-		</div>
-	</form>
-	<a href="#" class="btn btn--round btn--highlighted modal-save">Update</a>
-	<a href="#" class="btn btn--round btn modal-close">Cancel</a>
+        </div>
+    </form>
+    <a href="#" class="btn btn--round btn--highlighted modal-save">Update</a>
+    <a href="#" class="btn btn--round btn modal-close">Cancel</a>
 </div>
 
 <div class="modal" data-modal-id="services-modal">
-	<form action="#">
-		<div class="grid grid--double-pad cf">
-			<div class="form-divider cf">
-				<h3>Services</h3>
-				<div class="form-divider__icon form-divider__icon--modal">
-					<img src="{{ URL::asset('img/icons/insulation.png') }}" alt="">
-				</div>
-			</div>
+    <form action="#">
+        <div class="grid grid--double-pad cf">
+            <div class="form-divider cf">
+                <h3>Services</h3>
+                <div class="form-divider__icon form-divider__icon--modal">
+                    <img src="{{ URL::asset('img/icons/insulation.png') }}" alt="">
+                </div>
+            </div>
 
-			@include('form-sections.services', array('pageLocation' => 'results'))
+            @include('form-sections.services', array('pageLocation' => 'results'))
 
-		</div>
-	</form>
-	<a href="#" class="btn btn--round btn--highlighted modal-save">Update</a>
-	<a href="#" class="btn btn--round btn modal-close">Close</a>
+        </div>
+    </form>
+    <a href="#" class="btn btn--round btn--highlighted modal-save">Update</a>
+    <a href="#" class="btn btn--round btn modal-close">Close</a>
 </div>
 
 <div class="modal" data-modal-id="energy-modal">
-	<form action="#">
-		<div class="grid grid--double-pad cf">
-			<div class="form-divider cf">
-				<h3>Energy</h3>
-				<div class="form-divider__icon form-divider__icon--modal">
-					<img src="{{ URL::asset('img/icons/energy.png') }}" alt="">
-				</div>
-			</div>
+    <form action="#">
+        <div class="grid grid--double-pad cf">
+            <div class="form-divider cf">
+                <h3>Energy</h3>
+                <div class="form-divider__icon form-divider__icon--modal">
+                    <img src="{{ URL::asset('img/icons/energy.png') }}" alt="">
+                </div>
+            </div>
 
-			@include('form-sections.energy')
+            @include('form-sections.energy')
 
-		</div>
-	</form>
-	<a href="#" class="btn btn--round btn--highlighted modal-save">Update</a>
-	<a href="#" class="btn btn--round btn modal-close">Close</a>
+        </div>
+    </form>
+    <a href="#" class="btn btn--round btn--highlighted modal-save">Update</a>
+    <a href="#" class="btn btn--round btn modal-close">Close</a>
 </div>
 
 <div class="modal" data-modal-id="roof-modal">
-	<form action="#">
-		<div class="grid grid--double-pad cf">
-			<div class="form-divider cf">
-				<h3>Roof</h3>
-				<div class="form-divider__icon form-divider__icon--modal">
-					<img src="{{ URL::asset('img/icons/insulation.png') }}" alt="">
-				</div>
-			</div>
+    <form action="#">
+        <div class="grid grid--double-pad cf">
+            <div class="form-divider cf">
+                <h3>Roof</h3>
+                <div class="form-divider__icon form-divider__icon--modal">
+                    <img src="{{ URL::asset('img/icons/insulation.png') }}" alt="">
+                </div>
+            </div>
 
-			<div class="col-2">
-				<div class="input-field">
-					<label for="loft-insulation" class="input-field__field-title"><span class="input-order-num">9</span>How deep in the insulation in your loft?</label>
-					<select class="input-chosen-select input--rounded-top input--border-bot" id="loft-insulation" name="loft-insulation" placeholder="Select from the following..">
-						@include('common.select-options', array('formField' => "loft-insulation", 'options' => $protoolDefaults['loftInsulation']))
-						<option value="unknown">don't know.</option>
-					</select>
+            <div class="col-2">
+                <div class="input-field">
+                    <label for="loft-insulation" class="input-field__field-title"><span class="input-order-num">9</span>How deep in the insulation in your loft?</label>
+                    <select class="input-chosen-select input--rounded-top input--border-bot" id="loft-insulation" name="loft-insulation" placeholder="Select from the following..">
+                        @include('common.select-options', array('formField' => "loft-insulation", 'options' => $protoolDefaults['loftInsulation']))
+                        <option value="unknown">don't know.</option>
+                    </select>
 
-					<p class="input-field__info">If you're not sure about this we will estimate based on the age of your home</p>
-				</div>
-			</div>
-		</div>
-	</form>
-	<a href="#" class="btn btn--round btn--highlighted modal-save">Update</a>
-	<a href="#" class="btn btn--round btn modal-close">Close</a>
+                    <p class="input-field__info">If you're not sure about this we will estimate based on the age of your home</p>
+                </div>
+            </div>
+        </div>
+    </form>
+    <a href="#" class="btn btn--round btn--highlighted modal-save">Update</a>
+    <a href="#" class="btn btn--round btn modal-close">Close</a>
 </div>
 
 <script>
-	var results = <?php echo json_encode($resultsData);?>;
+    var results = <?php echo json_encode($resultsData); ?>;
 </script>
 @stop
